55808ed79654b428cc7a0f66a8cac43a29a6e570
bal3000 <bal3000286@googlemail.com>
2020-10-30 14:38:39 +0000

    first commit

---
+++ VLCStreamerTest/ChromecastHelper.cs
@@ -0,0 +1,59 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VLCStreamerTest
+{
+    public class ChromeCastHelper : IDisposable
+    {
+        private readonly LibVLC _libVLC;
+
+        public ChromeCastHelper()
+        {
+            // Load native libvlc library
+            Core.Initialize();
+            _libVLC = new LibVLC();
+            // Redirect log output to the console
+            _libVLC.Log += (sender, e) => Console.WriteLine($"[{e.Level}] {e.Module}:{e.Message}");
+
+            // Start finding chromecasts
+            DiscoverChromecasts();
+        }
+
+        public List<RendererItem> RendererItems { get; set; } = new List<RendererItem>();
+
+        public void DiscoverChromecasts()
+        {
+            var rendererDiscoverer = new RendererDiscoverer(_libVLC);
+            rendererDiscoverer.ItemAdded += RendererDiscoverer_ItemAdded;
+            rendererDiscoverer.Start();
+        }
+
+        public bool StartCasting(Uri stream, RendererItem rendererItem)
+        {
+            Console.WriteLine($"Starting cast with stream: {stream} on renderer {rendererItem.Name} or type {rendererItem.Type}");
+
+            using var media = new Media(_libVLC, stream);
+
+            using var mediaPlayer = new MediaPlayer(_libVLC);
+
+            mediaPlayer.SetRenderer(rendererItem);
+
+            // start the playback
+            return mediaPlayer.Play(media);
+        }
+
+        protected void RendererDiscoverer_ItemAdded(object sender, RendererDiscovererItemAddedEventArgs e)
+        {
+            Console.WriteLine($"New chromecast discovered: {e.RendererItem.Name} of type {e.RendererItem.Type}");
+
+            RendererItems.Add(e.RendererItem);
+        }
+
+        public void Dispose()
+        {
+            _libVLC.Dispose();
+        }
+    }
+}
---
+++ VLCStreamerTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VLCStreamerTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting app");
+            var helper = new ChromeCastHelper();
+
+            // hold on a bit at first to give libvlc time to find the chromecast
+            await Task.Delay(2000);
+
+            var chromecast = helper.RendererItems.FirstOrDefault();
+            var result = helper.StartCasting(new Uri(""), chromecast);
+
+            if (result)
+            {
+                Console.WriteLine("Successfully connected and streaming");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect to chromecast");
+            }
+        }
+    }
+}
